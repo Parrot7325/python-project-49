50cc7a5fbe41b54a24ee7a8bd8fa0df3ce9ebadc
Parrot7325 <user@uchebahexlet>
2022-10-31 17:46:27 +0300

    Выполнен 4-ый шаг

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
 def brain_games():
     print('Welcome to the Brain Games!')
+
+
 def main():
     brain_games()
+
 
 if __name__ == '__main__':
     main()
