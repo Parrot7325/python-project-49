3606e8abaabe24bdc6ef5b07a3631e64a6c5489a
Parrot7325 <maximus1997@yahoo.com>
2022-11-08 15:53:44 +0300

    game_logic добавлен в индекс

---
+++ brain_games/scripts/game_logic.py
@@ -0,0 +1,18 @@
+def right_or_wrong(quest_global, gen_right):
+    import sys
+    import prompt
+
+
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
+    print(f'Hello, {name}!\n{quest_global}')
+    won_games = 0
+    while won_games < 3:
+        rights = gen_right()
+        right_answer = rights[1]
+        answer = input(f'Question: {rights[0]}\nYour answer: ')
+        if answer == right_answer:
+            won_games += 1
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.\nLet's try again, {name}!")
+            sys.exit()
