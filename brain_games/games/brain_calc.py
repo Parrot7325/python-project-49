f2e95fbaa0f382aeb88b18d50ff4cc3de869279e
Parrot7325 <82766453+Parrot7325@users.noreply.github.com>
2022-11-14 21:25:55 +0300

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def choose_operation(x, y):
     doing = randint(0, 2)
     if doing == 0:
@@ -12,6 +13,7 @@ def choose_operation(x, y):
         result = x * y
         expression = f'{x} * {y}'
     return (expression, str(result))
+
 
 def calc_game():
     x = randint(0, 100)
