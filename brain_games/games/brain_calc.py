cb9c0ed9d4502a3d4a679556131d9f98f92f4508
Parrot7325 <82766453+Parrot7325@users.noreply.github.com>
2022-11-25 12:46:54 +0300

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -18,13 +18,6 @@ def make_question(first_number, second_number, doing):
     return f'{first_number} {doing} {second_number}'
 
 
-def choose_operation(first_number, second_number):
-    doing = choice(DOINGS)
-    result = make_result(first_number, second_number, doing)
-    expression = make_question(first_number, second_number, doing)
-    return expression, str(result)
-
-
 def calc_game():
     print('What is the result of the expression?')
     first_random_number = randint(0, 100)
