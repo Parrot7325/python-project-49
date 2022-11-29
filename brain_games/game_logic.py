ad801997330ede7ffa19a2c41c710faeb1a66dc9
Parrot7325 <maximus1997@yahoo.com>
2022-11-29 17:41:41 +0300

    Update game_logic

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -7,15 +7,14 @@ def game(gen_right):
     print(f'Hello, {name}!')
     won_games = 0
     while won_games < 3:
-        rights = gen_right()
-        right_answer = rights[1]
-        answer = input(f'Question: {rights[0]}\nYour answer: ')
-        if answer == right_answer:
+        question, correct_answer = gen_right()
+        answer = input(f'Question: {question}\nYour answer: ')
+        if answer == correct_answer:
             won_games += 1
             print('Correct!')
         else:
             print(f'''
-'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.
+'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
 Let's try again, {name}!''')
             sys.exit()
     print(f'Congratulations, {name}!')
