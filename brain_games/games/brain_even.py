f345c3a0e9a31d78e6d1c09733ca4df447d85df9
Parrot7325 <82766453+Parrot7325@users.noreply.github.com>
2022-12-03 15:03:24 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,17 +5,8 @@ def is_even(number):
     return number % 2 == 0
 
 
-<<<<<<< HEAD
-QUESTION = ('Answer "yes" if the number is even, '
-            'otherwise answer "no".')
-<<<<<<< HEAD
-=======
 QUESTION = '''
 Answer "yes" if the number is even, 'otherwise answer "no".'''
->>>>>>> parent of 34150f5 (Кортеж заменен на f-строки)
-=======
->>>>>>> 63ab1c2be87df2d5bdc7b20a1274fb11a5ee141e
-
 
 def even_game():
     random_number = randint(0, 1000)
