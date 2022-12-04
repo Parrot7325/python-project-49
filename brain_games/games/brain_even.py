a1eb4e02f79a70e8b46a5825d3f9b7809bf9e9fd
Parrot7325 <82766453+Parrot7325@users.noreply.github.com>
2022-12-04 22:06:17 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,8 +5,8 @@ def is_even(number):
     return number % 2 == 0
 
 
-QUESTION = '''
-Answer "yes" if the number is even, 'otherwise answer "no".'''
+QUESTION = ('Answer "yes" if the number is even, '
+            'otherwise answer "no".')
 
 def even_game():
     random_number = randint(0, 1000)
